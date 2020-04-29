31645e0f4685a186f2ebef71c7ad29c4053afcbc
chobe111 <chobe1@naver.com>
2020-04-29 15:38:38 +0900

    writing uNet structure

--- keras_utils.py
+++ keras_utils.py
@@ -3,23 +3,57 @@ from keras.layers import Conv2D
 from keras.layers import Conv2DTranspose
 from keras.layers import BatchNormalization
 from keras.layers import Dropout
+from keras.layers import MaxPooling2D
+from keras.layers import concatenate
 
 
-def encoder_conv1(filter_size: int, input: keras.layers) -> keras.layers:
-    conv1 = Conv2D(32, (3, 3), padding='same', activation='relu')(input)
-    conv1 = BatchNormalization()(conv1)
-    conv1 = Dropout(0.5)(conv1)
-
-    return conv1
+class tensor:
+    def __init__(self):
+        pass
 
 
-def encoder_conv2(filter_size: int, input: keras.layers) -> keras.layers:
-    pass
+def encoder_conv(filter_size: int, input: tensor) -> tuple[tensor, tensor]:
+    conv1 = Conv2D(filter_size, (3, 3), padding='same', activation='relu')(input)
+    batch1 = BatchNormalization()(conv1)
+    drop1 = Dropout(0.5)(batch1)
+
+    conv2 = Conv2D(filter_size, (3, 3), padding='same', activation='relu')(drop1)
+    batch2 = BatchNormalization()(conv2)
+
+    conv3 = Conv2D(filter_size, (3, 3), padding='same', activation='relu')(batch2)
+    batch3 = BatchNormalization()(conv3)
+    pool3 = MaxPooling2D((2, 2))(batch3)
+
+    return batch3, pool3
 
 
-def decoder_conv1():
-    pass
+def encoder_to_decoder_conv(filter_size: int, input: tensor) -> tensor:
+    conv1 = Conv2D(filter_size, (3, 3), padding='same', activation='relu')(input)
+    batch1 = BatchNormalization()(conv1)
+    drop1 = Dropout(0.5)(batch1)
+
+    conv2 = Conv2D(filter_size, (3, 3), padding='same', activation='relu')(drop1)
+    batch2 = BatchNormalization()(conv2)
+
+    up3 = Conv2DTranspose(int(filter_size / 2), (2, 2), padding='same', activation='relu')(batch2)
+
+    return up3
 
 
-def decoder_conv1():
+def decoder_conv(filter_size: int, input: tensor, merge_input: tensor) -> tensor:
+    merge1 = concatenate([input, merge_input], axis=3)
+
+    conv1 = Conv2D(filter_size, (3, 3), padding='same', activation='relu')(merge1)
+    batch1 = BatchNormalization()(conv1)
+    drop1 = Dropout(0.5)(batch1)
+
+    conv2 = Conv2D(filter_size, (3, 3), padding='same', activation='relu')(drop1)
+    batch2 = BatchNormalization()(conv2)
+
+    up3 = Conv2DTranspose(int(filter_size / 2), (2, 2), padding='same', activation='relu')(batch2)
+
+    return up3
+
+
+def generator_final_layer(filter_size):
     pass
--- model.py
+++ model.py
@@ -4,8 +4,9 @@ import keras.optimizers
 from keras_utils import *
 
 
-class Discriminator(object):
-    def __init__(self, input_size):
+class Discriminator(keras.models.Model):
+    def __init__(self, input_size, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.input_size = input_size
         self.inputs = keras.models.Input(input_size)
 
@@ -16,18 +17,28 @@ class Discriminator(object):
         pass
 
 
-class Generator(object):
-    def __init__(self, input_size):
+class Generator(keras.models.Model):
+    def __init__(self, input_size, *args, **kwargs):
         # input_shape = (256, 256, 1)
+        super().__init__(*args, **kwargs)
         self.input_size = input_size
         self.inputs = keras.models.Input(input_size)
 
     def _encoder(self):
-        conv1 = encoder_conv1(32, self.inputs)
-        conv1 = keras.layers.MaxPooling2D(conv1)
-        conv2 = encoder_conv1(64, conv1)
-        
+        batch1, pool1 = encoder_conv(32, self.inputs)
+        batch2, pool2 = encoder_conv(64, pool1)
+        batch3, pool3 = encoder_conv(128, pool2)
+        batch4, pool4 = encoder_conv(256, pool3)
+        batch5, pool5 = encoder_conv(512, pool4)
 
+        up1 = encoder_to_decoder_conv(1024, pool5)
+
+        up2 = decoder_conv(512, up1, batch5)
+        up3 = decoder_conv(256, up2, batch4)
+        up4 = decoder_conv(128, up3, batch3)
+        up5 = decoder_conv(64, up4, batch2)
+
+        generator_final_layer(32)
 
         pass
 
