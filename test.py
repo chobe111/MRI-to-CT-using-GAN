5b2696f7535356607b7ef043caa9ce4811a482ea
chobe111 <chobe1@naver.com>
2020-05-07 16:55:32 +0900

    just test

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
-from model import Generator
+from model import Generator, Discriminator
 from keras.utils import plot_model
 from keras.models import Model
 from keras.applications import VGG16
 
-generator = Generator((256, 256, 1))
-plot_model(generator, to_file="model3.png", show_shapes=True)
+generator = Generator((512, 512, 1))
+discriminator = Discriminator((352, 352, 1))
+
+plot_model(generator, to_file="model4.png", show_shapes=True)
+# plot_model(discriminator, to_file="dis_model.png", show_shapes=True)
