76ab0f83bb6059721c73008dfb0289c8b279337a
chobe111 <chobe1@naver.com>
2020-05-07 16:54:20 +0900

    complete discriminator

--- keras_utils.py
+++ keras_utils.py
@@ -6,15 +6,33 @@ from keras.layers import Dropout
 from keras.layers import MaxPooling2D
 from keras.layers import concatenate
 from keras.layers import Activation
+from keras.layers import Dense
 from tensorflow.python.framework.ops import Tensor
 
 
-def dis_conv(input: Tensor):
-    conv1 = Conv2D(2, (3, 3), )
+def discriminator_final_layer(input: Tensor) -> Tensor:
+    final_layer = Dense(1, activation='sigmoid')(input)
+    return final_layer
+
+
+def discriminator_dense(output_nodes: int, input: Tensor) -> Tensor:
+    dense1 = Dense(output_nodes, activation='relu')(input)
+    drop1 = Dropout(0.5)(dense1)
+
+    return drop1
+
+
+def discriminator_conv(filter_size: int, input: Tensor) -> Tensor:
+    conv = Conv2D(filter_size, (3, 3), padding='same', strides=(2, 2))(input)
+    batch = BatchNormalization()(conv)
+    activate = Activation('relu')(batch)
+
+    return conv
 
 
 def base_conv(filter_size: int, input: Tensor) -> Tensor:
     conv = Conv2D(filter_size, (3, 3), padding='same')(input)
+    # conv = Conv2D(filter_size, (3, 3), padding='valid')(input)
     batch = BatchNormalization()(conv)
     activate = Activation('relu')(batch)
 
@@ -32,7 +50,7 @@ def encoder_conv(filter_size: int, input: Tensor) -> (Tensor, Tensor):
     conv1 = dropout_conv(filter_size, input)
     conv2 = base_conv(filter_size, conv1)
     conv3 = Conv2D(filter_size, (3, 3), padding='same')(conv2)
-
+    # conv3 = Conv2D(filter_size, (3, 3), padding='valid')(conv2)
     batch3 = BatchNormalization()(conv3)
     activate3 = Activation('relu')(batch3)
     pool3 = MaxPooling2D((2, 2))(activate3)
@@ -47,6 +65,9 @@ def encoder_to_decoder_conv(filter_size: int, input: Tensor) -> Tensor:
     up3 = Conv2DTranspose(int(filter_size / 2), (2, 2), strides=(2, 2),
                           padding='same', activation='relu')(conv2)
 
+    # up3 = Conv2DTranspose(int(filter_size / 2), (2, 2), strides=(2, 2),
+    #                       padding='valid', activation='relu')(conv2)
+
     return up3
 
 
@@ -58,6 +79,8 @@ def decoder_conv(filter_size: int, input: Tensor, merge_input: Tensor) -> Tensor
 
     up3 = Conv2DTranspose(int(filter_size / 2), (2, 2), strides=(2, 2),
                           padding='same', activation='relu')(conv2)
+    # up3 = Conv2DTranspose(int(filter_size / 2), (2, 2), strides=(2, 2),
+    #                       padding='valid', activation='relu')(conv2)
 
     return up3
 
@@ -68,5 +91,6 @@ def generator_final_layer(filter_size, input: Tensor, merge_input: Tensor) -> Te
     conv1 = base_conv(filter_size, merge1)
     conv2 = base_conv(filter_size, conv1)
     conv3 = Conv2D(1, (1, 1), padding='same', activation='sigmoid')(conv2)
+    # conv3 = Conv2D(1, (1, 1), padding='valid', activation='sigmoid')(conv2)
 
     return conv3
