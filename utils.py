6b9647b4fe47cbd76001afd2e0be91ce70b06c83
chobe111 <chobe1@naver.com>
2020-05-12 16:09:38 +0900

    add Mutual Information function

--- utils.py
+++ utils.py
@@ -1,21 +1,74 @@
 from keras.losses import BinaryCrossentropy
 import tensorflow as tf
+import numpy as np
+from scipy import ndimage
 
 
 class GanLosses:
     def __init__(self):
         self.binary_loss_obj = BinaryCrossentropy(from_logits=True)
 
-    def discriminator_loss(self, real_image, generated_image):
-        real_loss = self.binary_loss_obj(tf.ones_like(real_image), real_image)
-        generated_image = self.binary_loss_obj(tf.zeros_like(generated_image), generated_image)
+    def discriminator_loss(self, real, generated):
+        real_loss = self.binary_loss_obj(tf.ones_like(real), real)
+        generated_image = self.binary_loss_obj(tf.zeros_like(generated), generated)
 
         total_loss = 0.5 * (real_loss + generated_image)
 
         return total_loss
 
-    def generator_loss(self):
-        return
+    def generator_loss(self, real_image, generated_image):
+
+        generated_loss = self.binary_loss_obj(tf.ones_like(generated_image), generated_image)
+        # Regulation with MI_LOSS
+
+        mi_loss = self.mutual_information_2d(real_image.ravel(), generated_image.ravel())
+
+        return mi_loss + generated_loss
+
+    @staticmethod
+    def mutual_information_2d(x, y, sigma=1, normalized=False):
+        """
+            Computes (normalized) mutual information between two 1D variate from a
+            joint histogram.
+            Parameters
+            ----------
+            x : 1D array
+                first variable
+            y : 1D array
+                second variable
+            sigma: float
+                sigma for Gaussian smoothing of the joint histogram
+            Returns
+            -------
+            nmi: float
+                the computed similariy measure
+            """
+        EPS = np.finfo(float).eps
+
+        bins = (256, 256)
+        jh = np.histogram2d(x, y, bins=bins)[0]
+        # smooth the jh with a gaussian filter of given sigma
+        ndimage.gaussian_filter(jh, sigma=sigma, mode='constant',
+                                output=jh)
+        # compute marginal histograms
+        jh = jh + EPS
+        sh = np.sum(jh)
+        jh = jh / sh
+        s1 = np.sum(jh, axis=0).reshape((-1, jh.shape[0]))
+        s2 = np.sum(jh, axis=1).reshape((jh.shape[1], -1))
+
+        # Normalised Mutual Information of:
+        # Studholme,  jhill & jhawkes (1998).
+        # "A normalized entropy measure of 3-D medical image alignment".
+        # in Proc. Medical Imaging 1998, vol. 3338, San Diego, CA, pp. 132-143.
+        if normalized:
+            mi = ((np.sum(s1 * np.log(s1)) + np.sum(s2 * np.log(s2)))
+                  / np.sum(jh * np.log(jh))) - 1
+        else:
+            mi = (np.sum(jh * np.log(jh)) - np.sum(s1 * np.log(s1))
+                  - np.sum(s2 * np.log(s2)))
+
+        return mi
 
     def __call__(self, name):
-        return
+        pass
