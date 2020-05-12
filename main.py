5705fc8e52aea1e45543e7a1ba5be7a77c42c458
chobe111 <chobe1@naver.com>
2020-05-12 16:18:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
 from keras.losses import BinaryCrossentropy
+import argparse
 
-loss_obj = BinaryCrossentropy(from_logits=True)
+parser = argparse.ArgumentParser(description="Implementation of MRI-only brain radiotherapy")
+
+
+def main():
+    """
+    This repository is implementation of MRI-only brain radiotherapy: assessing the
+    dosimetric accuracy of synthetic CT images generated using a deep learning approach
+
+    Dissertation writer : Samaneh Kazemifar, Sarah McGuire, Robert Timmerman, Zabi Wardak,
+                        Dan Nguyen, Yang Park, Steve Jiang, Amir Owrangi at Medical Artificial
+                         Intelligence and Automation Laboratory, Department of Radiation Oncology,
+                          University of Texas Southwestern, Dallas, Texas, US
+
+    writer email : Amir.Owrangi@UTSouthwestern.edu
+
+    coder writer : Myungki Cho Inha University in South Korea
+    coder email : chobe0719@gmail.com
+    """
+    return
+
+
+if __name__ == '__main__':
+    main()
