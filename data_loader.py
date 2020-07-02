ae2e9b8a9206b6d0ea2dd40a2f7a138d453c8faf
chobe111 <chobe1@naver.com>
2020-07-02 16:09:19 +0900

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -3,7 +3,10 @@ import tensorflow as tf
 
 
 class DataLoader:
-    def __init__(self, tfrecord_file_path, image_size=(512, 512, 1), ):
+    def __init__(self, data_path, image_size=(512, 512, 1), name='', batch_size=32, is_train=True):
+        self.ori_img_size = image_size
+        self.pair_img_size = (image_size.shape[0], image_size.shape[1] * 2, image_size.shape[2])
+
         pass
 
     def __call__(self, *args, **kwargs):
